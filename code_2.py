55df7d1af3ee0e6e3b3f29c22adb8fb10efb132b
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-15 10:50:38 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,28 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        }
+public class RandomDataGenerator {
+        }
+        int[] data = new int[10];
+
+        for (int item : data) {
+
+    }    const data = [];
+}
+    for (let i = 0; i < 10; i++) {
+    }
+    data.forEach(item => {
+
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+function generateRandomData() {
+}
+    return data;
+function main() {

373aa16a79df6f30907d37c48ec8865ea4417206
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-15 08:38:05 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        }
+        }
+    public static void main(String[] args) {
+        int[] data = new int[10];
+}
+        for (int i = 0; i < 10; i++) {

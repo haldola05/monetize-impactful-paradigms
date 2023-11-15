13edb5410678234afda32f34d812e1848d6c037d
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-15 09:44:24 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,39 @@
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        }
         for (int item : data) {
 
 
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-        }
-        }
     public static void main(String[] args) {
-        int[] data = new int[10];
-}
+        }
         for (int i = 0; i < 10; i++) {
+    }
+}
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;    for item in data:
+        print(f"Random Number: {item}")
+
+    main()
+    data = generate_random_data()
+
+
+    return data
+def generate_random_data():
+if __name__ == "__main__":
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+        print(f"Random Number: {item}")
+    return data
+
+if __name__ == "__main__":
+def main():
+def generate_random_data():
+
+    for item in data:
+    data = generate_random_data()
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]

38772d7319d3d86ab70d99577d542c212a374f60
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-15 11:12:41 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,28 +1,56 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-        }
-public class RandomDataGenerator {
-        }
-        int[] data = new int[10];
 
-        for (int item : data) {
+def generate_random_data():
+    main()
+if __name__ == "__main__":
 
-    }    const data = [];
-}
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+def main():
+
+
+if __name__ == "__main__":
+    for item in data:
+
+def main():
+    main()
+import random
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    return data;
+
+        console.log("Random Number: " + item);
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
     for (let i = 0; i < 10; i++) {
+    });
+main();
+    const data = [];
+}
     }
     data.forEach(item => {
-
     const data = generateRandomData();
-        console.log("Random Number: " + item);
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-main();
-function generateRandomData() {
 }
-    return data;
-function main() {
+function generateRandomData() {
+    main()
+    return data
+
+def main():
+def generate_random_data():
+
+
+import random
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    for item in data:
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
